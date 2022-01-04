4c45d80e419dd44f44f09811cbd61b82b080c5df
Yashar Akish <yashar_akish@yahoo.com>
2022-01-03 20:13:11 -0500

    used dynamic styles in css of styled components instead of inline

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -13,12 +13,14 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${props => props.invalid ? 'red': ''}
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${props => (props.invalid ? 'red': '#ccc')};
+    background: ${props => props.invalid ? '#ffd7d7' : ''};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -29,15 +31,7 @@ const FormControl = styled.div`
     background: #fad0ec;
     border-color: #8b005d;
   }
-
-  &.invalid input{
-    border-color:red;
-    background: #ffd7d7;
-  }
-
-  &.invalid label{
-    color:red;
-  }`
+`
 ;
 
 const CourseInput = props => {
@@ -63,7 +57,7 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}> */}
-      <FormControl className={!isValid && 'invalid'}>
+      <FormControl invalid={!isValid}>
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
